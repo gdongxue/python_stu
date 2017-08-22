64b025320cdac7d79189384374491ba9d0299946
lizhi1115 <714131254@qq.com>
2017-08-22 23:44:53 +0800

    shop

--- CreateOrder.py
+++ CreateOrder.py
@@ -5,18 +5,16 @@ import Login
 import json
 import HTMLTestRunner
 import Adress
-
-class CreateOrder(unittest.TestCase):
-  for i in range(10):
-    if i <10:
+class CreateOrder():
+    # 创建订单
+     curl = "http://api.100iec.com/order/createOrder"
+     # 订单详情
+     ourl = "http://api.100iec.com/order/orderinfo"
      def setUp(self):
-        # 创建订单
-        self.curl = "http://api.100iec.com/order/createOrder"
-        # 订单详情
-        self.ourl = "http://api.100iec.com/order/orderinfo"
         self.login = Login.login()
         self.cookie = self.login.cookies
      def create(self):
+
         data = {
             "dispatchtype": 0,
             "addressid": 1751,
@@ -25,17 +23,16 @@ class CreateOrder(unittest.TestCase):
             "carrier": 1,
             "deduct2":1
         }
-        order = requests.post(self.curl, data=data, cookies=self.cookie)
+        self.setUp()
+        order = requests.post(url=self.curl, data=data, cookies=self.cookie)
         # 将字符串转换为字典
         res = json.loads(order.text)
         # 获取ordersn
         ordersn = res["result"]["ordersn"]
         print ordersn
-        return ordersn
-    else:
-        break
+        return order
 
-    def test_order_message(self):
+     def order_message(self):
         ordersn = self.create()
         data = {
             "ordersn":ordersn
@@ -45,20 +42,12 @@ class CreateOrder(unittest.TestCase):
         message = json.loads(om.text)
         result = message["result"]
         print result
-        # flag = 0
-        # for i in  result:
-        #     if (i == "U170711166074675802"):
-        #         print "下单人正确"
-        #         break
-        #     else:
-        #         print "下单人不正确"
-        #
-        # if(flag==0):
-        #         print "下单人正确"
-    def tearDown(self):
+
+     def tearDown(self):
         pass
+
 if __name__ == '__main__':
-    unittest.main()
+    CreateOrder()
 
 
 
