05c12f4126ef1c3d96835f3a4db9e34dd34ac75b
gdongxue <gaodongxue1991@sina.com>
2017-12-13 22:56:57 +0800

    shop

--- test_case/test_yagmail.py
+++ test_case/test_yagmail.py
@@ -3,3 +3,4 @@ import yagmail
 yag = yagmail.SMTP(user="1564260446@qq.com",password="jbjvvuhiqfjmhjea",host="smtp.qq.com")
 contents=[u"立志,你就是个大傻子！哈哈哈哈....."]
 yag.send("zglizhi@126.com",u"我发的邮件",contents=contents,)
+print u"邮件发送成功"
